20ce68543fd1f56721f67be1b2c0108ef7117d3f
AnitM <anit.mangal@gmail.com>
2024-04-05 02:16:25 +0530

    Init Assgn8

---
+++ Assign8/master.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int k, m, f;
+    printf("Enter number of processes: ");
+    scanf("%d", &k);
+    printf("Enter virtual space size (Max number of pages per process): ");
+    scanf("%d", &m);
+    printf("Enter number of frames: ");
+    scanf("%d", &f);      
+}
